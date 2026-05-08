54d2b381036f5d0e4bc94c66077a393c161a20d5
meirelleshugo <hugo.meirelles2002@gmail.com>
2026-05-08 17:15:35 -0300

    docs: add Swagger API documentation
    Configure Swagger UI and document dinosaur API endpoints

--- main.ts
+++ main.ts
@@ -1,17 +1,77 @@
 import { type Request, type Response } from "express";
 import data from "./data.json" with { type: "json" };
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
 import express from "express";
 
 const app = express();
 
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "FlowSync Engine API",
+      version: "1.0.0",
+      description:
+        "Branch Governance & Synchronization Platform API Documentation",
+    },
+    servers: [
+      {
+        url: "http://localhost:8000",
+      },
+    ],
+  },
+  apis: ["./main.ts"],
+};
+
+const specs = swaggerJsdoc(swaggerOptions);
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Welcome route
+ *     responses:
+ *       200:
+ *         description: Returns welcome message
+ */
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the Dinosaur API!");
 });
 
+/**
+ * @swagger
+ * /api:
+ *   get:
+ *     summary: Get all dinosaurs
+ *     responses:
+ *       200:
+ *         description: Returns all dinosaurs
+ */
 app.get("/api", (req: Request, res: Response) => {
   res.send(data);
 });
 
+/**
+ * @swagger
+ * /api/{dinosaur}:
+ *   get:
+ *     summary: Get a dinosaur by name
+ *     parameters:
+ *       - in: path
+ *         name: dinosaur
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Dinosaur name
+ *     responses:
+ *       200:
+ *         description: Dinosaur found
+ *       404:
+ *         description: Dinosaur not found
+ */
 app.get("/api/:dinosaur", (req: Request, res: Response) => {
   if (req?.params?.dinosaur) {
     const found = data.find(
