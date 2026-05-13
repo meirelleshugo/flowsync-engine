d9aa005f6206ed9370317cda6a4185480ab3a8d6
meirelleshugo <hugo.meirelles2002@gmail.com>
2026-05-13 16:33:11 -0300

    refactor: remove dinosaur module

--- src/modules/dinosaur/index.ts
+++
@@ -1,7 +0,0 @@
-import DinosaurRepository from "./dinosaur.repository.ts";
-
-import DinosaurService from "./dinosaur.service.ts";
-
-export const DinosaurRepositoryImp = new DinosaurRepository();
-
-export const DinosaurServiceImp = new DinosaurService(DinosaurRepositoryImp);
