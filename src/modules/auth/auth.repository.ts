bd1fe17b26fd286db46729df06175ead8d3081db
meirelleshugo <hugo.meirelles2002@gmail.com>
2026-05-15 11:32:04 -0300

    refactor: move auth and user models and repositories to models layer

--- src/modules/auth/auth.repository.ts
+++
@@ -1,9 +0,0 @@
-import UsersModel from "../users/users.model.ts";
-
-import { UsersRepositoryImp } from "../users/index.ts";
-
-export default class AuthRepository {
-  async findByEmail(user: UsersModel): Promise<UsersModel | null> {
-    return await UsersRepositoryImp.findByEmail(user.email);
-  }
-}
